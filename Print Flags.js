beebd00a9c28314bb7daccc35fce838bef2edcd8
GokulRSG <gokulnath2718@gmail.com>
2023-10-20 11:27:50 +0530

    Day 3 Task

--- Print Flags.js
+++ Print Flags.js
@@ -1,4 +1,14 @@
 let URL = "https://restcountries.com/v3.1/all";
 
-XMLHttpRequest req = new XMLHttpRequest;
-req.
+const req = new XMLHttpRequest;
+req.addEventListener("load", function(){
+    let obj = JSON.parse(this.response);
+    for(let arr of obj)
+        {
+            console.log(arr.flags.png)
+        }
+    //console.log(this.response);
+});
+
+req.open("GET", URL);
+req.send();
